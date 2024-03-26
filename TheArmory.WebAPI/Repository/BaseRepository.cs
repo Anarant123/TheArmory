421a40eacbe1bc87152ff23fda23cba7bcd07ab0
Mikhail Safronov <misha.safronov.02.12@mail.ru>
2024-03-26 20:38:46 +0300

    Базовый класс репозитория

--- TheArmory.WebAPI/Repository/BaseRepository.cs
+++ TheArmory.WebAPI/Repository/BaseRepository.cs
@@ -1,6 +1,23 @@
-﻿namespace TheArmory.Repository;
+﻿using TheArmory.Domain.Context;
+using TheArmory.Domain.Models.Database;
+
+namespace TheArmory.Repository;
 
 public class BaseRepository
 {
+    protected readonly ApplicationContext Context;
+    protected readonly ILogger<BaseRepository> Logger;
     
+    public BaseRepository(ApplicationContext context, ILogger<BaseRepository> logger)
+    {
+        Context = context;
+        Logger = logger;
+    }
+}
+
+public class BaseRepository<TEntity> : BaseRepository where TEntity : DbEntity
+{
+    public BaseRepository(ApplicationContext context, ILogger<BaseRepository> logger) : base(context, logger)
+    {
+    }
 }
