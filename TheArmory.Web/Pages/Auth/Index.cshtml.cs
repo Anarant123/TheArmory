9cbe9fcffc5a0a1f619de1754d86c8ab590c7487
Mikhail Safronov <misha.safronov.02.12@mail.ru>
2024-04-08 22:36:14 +0300

    заготовки страниц авторизации и регистрации

--- TheArmory.Web/Pages/Auth/Index.cshtml.cs
+++ TheArmory.Web/Pages/Auth/Index.cshtml.cs
@@ -1,19 +1,44 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TheArmory.Domain.Models.Request.Commands.User;
+using TheArmory.Web.Service;
 
 namespace TheArmory.Web.Pages.Auth;
 
 public class Index : PageModel
 {
-    // todo поменять на модель комманды
-    [Required(ErrorMessage = "Поле 'Логин' обязательно для заполнения")]
-    public string Login { get; set; }
+    private readonly AuthService _service;
+    
+    [BindProperty]
+    public UserLoginCommand Command { get; set; }
 
-    [Required(ErrorMessage = "Поле 'Пароль' обязательно для заполнения")]
-    public string Password { get; set; }
+    public Index(AuthService service)
+    {
+        _service = service;
+    }
     
     public void OnGet()
     {
         
+    }
+    
+    public async Task<IActionResult> OnPostAsync(UserLoginCommand command)
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        var result = await _service.Login(Command);
+        if (result?.Item != null)
+        {
+            return RedirectToPage("/Account/Index");
+        }
+        else
+        {
+            ModelState.AddModelError(string.Empty, "Неверный логин или пароль.");
+            return Page();
+        }
     }
 }
--- TheArmory.Web/Pages/Auth/Registration.cshtml.cs
+++ TheArmory.Web/Pages/Auth/Registration.cshtml.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using TheArmory.Domain.Models.Request.Commands.User;
 
 namespace TheArmory.Web.Pages.Auth;
 
 public class Registration : PageModel
 {
+    public UserCreateCommand Command;
     public void OnGet()
     {
         
