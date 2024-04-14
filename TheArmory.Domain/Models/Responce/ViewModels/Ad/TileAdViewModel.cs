d782756b4321ed4c3a880deea3fc799ab0b4f319
Mikhail Safronov <misha.safronov.02.12@mail.ru>
2024-04-14 15:31:01 +0300

    модель плитки объявлений

--- TheArmory.Domain/Models/Responce/ViewModels/Ad/TileAdViewModel.cs
+++ TheArmory.Domain/Models/Responce/ViewModels/Ad/TileAdViewModel.cs
@@ -1,6 +1,23 @@
-﻿namespace TheArmory.Domain.Models.Responce.ViewModels.Ad;
+﻿using TheArmory.Domain.Models.Database;
+
+namespace TheArmory.Domain.Models.Responce.ViewModels.Ad;
 
 public class TileAdViewModel
 {
+    public Guid Id { get; set; }
     
+    public List<Media> Images { get; set; }
+    
+    public decimal Price { get; set; }
+    
+    public DateTime CreationDateTime { get; set; }
+
+
+    public TileAdViewModel(Database.Ad ad)
+    {
+        Id = ad.Id;
+        Images = ad.Medias;
+        Price = ad.Price;
+        CreationDateTime = ad.CreationDateTime;
+    }
 }
